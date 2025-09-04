29ebf65876add321664ecbc58107ec7883a6dab0
MegeKaplan <kaplanmehmetege@gmail.com>
2025-09-04 14:33:51 +0300

    refactor: replace home page content with redirect to /home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,5 @@
-"use client";
-import { Button } from "@/components/ui/button"
-import { useTranslations } from "next-intl";
-import { useLocaleChanger } from "@/hooks/useLocaleChanger";
-import { useThemeChanger } from "@/hooks/useThemeChanger";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  const t = useTranslations('Home');
-  const { changeLocale } = useLocaleChanger();
-  const { changeTheme } = useThemeChanger();
+const RedirectHomePage = () => redirect("/home");
 
-  return (
-    <div className="flex items-center justify-center flex-col h-screen">
-      <h1>{t("welcome")}</h1>
-      <Button>{t("clickMe")}</Button>
-      <div className="flex space-x-2 mt-4">
-        <Button onClick={() => changeTheme("dark")}>{t("darkMode")}</Button>
-        <Button onClick={() => changeTheme("light")}>{t("lightMode")}</Button>
-      </div>
-      <div className="flex space-x-2 mt-4">
-        <Button onClick={() => changeLocale('en')}>{t("english")}</Button>
-        <Button onClick={() => changeLocale('tr')}>{t("turkish")}</Button>
-      </div>
-    </div>
-  );
-}
+export default RedirectHomePage;
