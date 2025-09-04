28eb71d35d9d8c4a857a41dc835f3e4ffef94bb4
MegeKaplan <kaplanmehmetege@gmail.com>
2025-09-04 17:33:30 +0300

    feat: add optional size value to LoadingAnimation component

--- src/components/LoadingAnimation.tsx
+++ src/components/LoadingAnimation.tsx
@@ -1,8 +1,8 @@
-const LoadingAnimation = () => {
+const LoadingAnimation = ({ size = 16 }: { size?: number }) => {
   return (
     <div className="flex items-center justify-center relative animate-ping">
-      <div className="absolute animate-spin rounded-full size-32 border-t-2 border-b-2 border-zinc-800"></div>
-      <div className="absolute animate-ping rounded-full size-16 border-2 border-zinc-800"></div>
+      <div className={`absolute animate-spin rounded-full size-${size * 2} border-t-2 border-b-2 border-zinc-800`}></div>
+      <div className={`absolute animate-ping rounded-full size-${size} border-2 border-zinc-800`}></div>
     </div>
   );
 };
