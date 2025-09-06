5febf47f8e49516f9bca17ab45c38daaa00ebee8
MegeKaplan <kaplanmehmetege@gmail.com>
2025-09-06 18:43:01 +0300

    fix: disable image optimization in next.config.ts to resolve production error

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,7 @@ const nextConfig: NextConfig = {
         hostname: process.env.NEXT_PUBLIC_MEDIA_HOST || 'localhost',
       },
     ],
+    unoptimized: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
